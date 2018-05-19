77c7f0a9f33808d237304ab917d54e9c62f1ff74
shubhamnegicoder <shubhn.201011@gmail.com>
2018-05-19 17:01:35 +0530

    country updated

--- src/views/Location/Country/Country.jsx
+++ src/views/Location/Country/Country.jsx
@@ -9,112 +9,110 @@ import axios from 'axios';
 // import Modal from 'react-responsive-modal';
 
 import { RegularCard, Table, ItemGrid } from "components";
- var data=[];
- class Country extends React.Component 
- {
-     constructor(props){
-         super(props);
-       
-         this.state={
-             load:false,
-             mydata:[],
-             countryName:"",
-             countryCode:"",
-             _id:"",
-             
-             
-         }
-     };
+var data = [];
+class Country extends React.Component {
+    constructor(props) {
+        super(props);
 
-clickaction=()=>{
-    this.setState({ countryName: "", countryCode: "", _id:""})    
-this.setState({load:true,options:"add"})
-}
+        this.state = {
+            load: false,
+            mydata: [],
+            countryName: "",
+            countryCode: "",
+            _id: "",
+            userId: "",
+            iActive:false
+             
 
-handleEdit=(e,item)=>{
+        }
+    };
+
+    clickaction = () => {
+        this.setState({ countryName: "", countryCode: "", _id: "" })
+        this.setState({ load: true, options: "add" })
+    }
+
+    handleEdit = (e, item) => {
+        console.log(item.isActive,"activa")
         this.setState({ load: true })
-    this.setState({countryName: item.countryName ,countryCode: item.countryCode , _id: item._id
-    ,options:"edit"})
-    
-    }    
-
-
-     data = () => {
-         axios.get("http://localhost:8080/allCountry?id=5af170d60c06c02559273df1")
-        .then(
-             (result) => {
-
-                 var maindata = [];
-                 var localdata = []
-                 console.log(result.data)
-
-                 result.data.data && result.data.data.map((item, key) => {
-                     localdata.push(item.countryCode, item.countryName)
-                     localdata.push(<button onClick={(e)=>this.handleEdit(e ,item) }>Edit</button>)
-                     maindata.push(localdata);
-                     localdata = [];
-                 })
-
-
-                 this.setState({ mydata: maindata })
-                 console.log(this.state.mydata, "arrsy")
-             },
-             (error) => {
-                 console.log("error", error)
-             }
-         )
-     }
-componentDidMount(){
-this.data()
-
-}
-onClose = () => {
- this.setState({load:false });
- this.data();
-
-};
-     handlePageChange=(pageNumber)=> 
-     {
-         console.log(pageNumber,)
-         this.setState({ activePage:pageNumber});
-     }
-render(){
-    return (
-        <Grid container>
-            <ItemGrid xs={12} sm={12} md={12}>
-                
-                <RegularCard
-                    plainCard
-                    cardTitle="Country List"
-                    cardSubtitle={
-                        <Button style={{float:"right"}}variant="fab" color="primary" aria-label="add" onClick={this.clickaction} >
-                            <AddIcon />
-                        </Button>
-                    }
-                    content={
-                        <Table
-                            tableHeaderColor="primary"
-                            tableHead={["CountryCode", "Country","Operation"]}
-                            tableData={this.state.mydata}
-                        />
-                        }
-                
-                />
-
-            </ItemGrid>
-            <Pagination
-                activePage={this.state.activePage}
-                itemsCountPerPage={5}
-                totalItemsCount={50}
-                pageRangeDisplayed={3}
-                onChange={this.handlePageChange}
-          />
-            <Modal open={this.state.load} data={{"_id":this.state._id,"countryName":this.state.countryName,"countryCode":this.state.countryCode}} onClose={this.onClose}/>
-        </Grid>
+        this.setState({
+            countryName: item.countryName, countryCode: item.countryCode, _id: item._id, userId: item.createdBy
+        })
        
-    );
-  
+    }
+
+
+    data = () => {
+        axios.get("http://localhost:8080/allCountry?id=5af170d60c06c02559273df1")
+            .then(
+                (result) => {
+
+                    var maindata = [];
+                    var localdata = []
+                    console.log(result.data)
+
+                    result.data.data && result.data.data.map((item, key) => {
+                        console.log(item.isActive,"active")
+                        localdata.push(item.countryCode, item.countryName,item.isActive?"True":"False")
+                        localdata.push(<button onClick={(e) => this.handleEdit(e, item)}>Edit</button>)
+                        maindata.push(localdata);
+                        localdata = [];
+                    })
+
+
+                    this.setState({ mydata: maindata })
+                    console.log(this.state.mydata, "arrsy")
+                },
+                (error) => {
+                    console.log("error", error)
+                }
+            )
+    }
+    componentDidMount() {
+        this.data()
+
+    }
+    onClose = () => {
+        this.setState({ load: false });
+        this.data();
+
+    };
+    handlePageChange = (pageNumber) => {
+        console.log(pageNumber, )
+        this.setState({ activePage: pageNumber });
+    }
+    render() {
+        
+        return (
+            <Grid container>
+                <ItemGrid xs={12} sm={12} md={12}>
+
+                    <RegularCard
+                        plainCard
+                        cardTitle="Country List"
+                        cardSubtitle={
+                            <Button style={{ float: "right" }} variant="fab" color="primary" aria-label="add" onClick={this.clickaction} >
+                                <AddIcon />
+                            </Button>
+                        }
+                        content={
+                            <Table
+                                tableHeaderColor="primary"
+                                tableHead={["CountryCode", "Country", "Is Active","Operation"]}
+                                tableData={this.state.mydata}
+                            />
+                        }
+
+                    />
+
+                </ItemGrid>
+
+                <Modal open={this.state.load} data={{ "_id": this.state._id, "countryName": this.state.countryName, "countryCode": this.state.countryCode, "userId": this.state.userId }} onClose={this.onClose} />
+            </Grid>
+
+        );
+
+    }
 }
- }
 
 export default Country;
